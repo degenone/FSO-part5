61f7f21061ada0e76266b4dccc96f391a96d92fb
Tero Kilpeläinen <degenone1@gmail.com>
2023-10-30 15:04:32 +0200

    update to exercise 5.2

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -32,6 +32,7 @@ const App = () => {
                 <>
                     <div className='titlePanel'>
                         <h2>My Blogs List</h2>
+                        <p>User: {user.username}</p>
                         <button type='button' onClick={handleLogout}>
                             Log Out
                         </button>
