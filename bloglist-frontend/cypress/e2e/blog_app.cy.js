fab0b7c7d7529798df718681e865f5c3f21064d7
Tero Kilpeläinen <degenone1@gmail.com>
2023-11-01 18:02:18 +0200

    exercise 5.18 done

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -4,10 +4,37 @@ describe('Blog app', () => {
         cy.visit('');
     });
 
-    it('Login form is showing', function() {
+    it('Login form is showing', function () {
         cy.contains('Log in to the Bloglist Application');
         cy.get('#username');
         cy.get('#password');
         cy.get('#btn-login');
+    });
+
+    describe('Login', function() {
+        const user = {
+            username: 'testuser',
+            name: 'Test User',
+            password: 'passworD|123',
+        };
+        beforeEach(function() {
+            cy.request('POST', `${Cypress.env('URL')}/users`, user);
+        });
+
+        it('succeed logging in', function() {
+            cy.get('#username').type(user.username);
+            cy.get('#password').type(user.password);
+            cy.get('#btn-login').click();
+            cy.contains(user.name);
+        });
+
+        it('fail logging in', function() {
+            cy.get('#username').type(user.username);
+            cy.get('#password').type('badpass1');
+            cy.get('#btn-login').click();
+            cy.get('.error').as('error');
+            cy.get('@error').should('contain', 'Check your credentials.');
+            cy.get('@error').should('have.css', 'color', 'rgb(178, 34, 34)');
+        });
     });
 });
